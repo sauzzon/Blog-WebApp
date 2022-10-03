501b275a0d084470549c1f4760810ddf1b5d5cb9
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-10-03 09:42:16 +0545

    folders and routes structured

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,37 @@
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Write from "./pages/Write";
+import Single from "./pages/Single";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/single",
+    element: <Single />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
