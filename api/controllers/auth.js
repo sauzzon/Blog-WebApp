37347cd0a0338185c9db6964143f2c2a1268a04f
sauzzon <saujan.tiwari@gmail.com>
2022-10-15 20:18:50 +0545

    login functionality implemented

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   // check existing user
@@ -23,6 +24,42 @@ export const register = (req, res) => {
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  // check user
+
+  const q = "SELECT * FROM users WHERE username = ?";
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.json(err);
+
+    if (data.length === 0) return res.status(404).json("User not found");
+
+    // check password
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong username or password");
+
+    //jwt
+    const token = jwt.sign(
+      {
+        id: data[0].id,
+      },
+      "jwt_secret_key"
+    );
+
+    const { password, ...other } = data[0];
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other); // send other info except password
+  });
+};
 
 export const logout = (req, res) => {};
