36db8b5d08277d76bd88f4f44409408b9cbcddcc
sauzzon <saujan.tiwari@gmail.com>
2022-10-16 21:03:25 +0545

    Add and update posts

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -21,7 +21,7 @@ const Menu = ({ cat }) => {
       <h1>Other post you may like</h1>
       {posts.map((post) => (
         <div className="post" key={post.id}>
-          <img src={post.img} alt="" />
+          <img src={`../upload/${post.img}`} alt="" />
           <h2>{post.title}</h2>
           <button>Read More</button>
         </div>
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -30,6 +30,11 @@ const Single = () => {
     fetchData();
   }, [postId]);
 
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    return doc.body.textContent;
+  };
+
   const handleDelete = async () => {
     try {
       await axios.delete(`/posts/${postId}`);
@@ -42,7 +47,7 @@ const Single = () => {
   return (
     <div className="single">
       <div className="content">
-        <img src={post?.img} alt="" />
+        <img src={`../upload/${post?.img}`} alt="" />
         <div className="user">
           {post.userImg && <img src={post.userImg} alt="" />}
           <div className="info">
@@ -51,7 +56,7 @@ const Single = () => {
           </div>
           {currentUser.username === post.username && (
             <div className="edit">
-              <Link to={`/write?edit=2`}>
+              <Link to={`/write?edit=2`} state={post}>
                 <img src={Edit} alt="" />
               </Link>
               <img onClick={handleDelete} src={Delete} alt="" />
@@ -59,7 +64,7 @@ const Single = () => {
           )}
         </div>
         <h1>{post.title}</h1>
-        {post.desc}
+        {getText(post.desc)}
       </div>
       <Menu cat={post.cat} />
     </div>
